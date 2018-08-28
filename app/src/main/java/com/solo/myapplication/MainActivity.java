2376cb882fd02b0704a21fe3ce39b4f07f42b22d
lisi <lee@qq.com>
2018-08-28 22:58:43 +0800

    wanjie

--- app/src/main/java/com/solo/myapplication/MainActivity.java
+++ app/src/main/java/com/solo/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        finish();
     }
 }
