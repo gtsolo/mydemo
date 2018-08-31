7b32f70e34484f6830ae6e95df0d8c60097bd2de
lisi <lee@qq.com>
2018-09-01 00:45:24 +0800

    splash

--- app/src/main/java/com/solo/myapplication/SplashActivity.java
+++ app/src/main/java/com/solo/myapplication/SplashActivity.java
@@ -1,15 +1,13 @@
 package com.solo.myapplication;
 
-import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        setContentView(R.layout.activity_splash);
     }
 }
