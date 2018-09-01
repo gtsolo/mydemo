1fe7ced68406cb94cb44f6c9698cdae260c225dd
lisi <lee@qq.com>
2018-09-01 12:32:06 +0800

    欢迎界面

--- app/src/main/java/com/solo/myapplication/SplashActivity.java
+++ app/src/main/java/com/solo/myapplication/SplashActivity.java
@@ -1,13 +1,45 @@
 package com.solo.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SplashActivity extends AppCompatActivity {
-
+    TextView tv_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        init();
+    }
+
+    private void init() {
+      tv_version= (TextView) findViewById(R.id.tv_version);
+        try {
+            PackageInfo packinfo =getPackageManager().getPackageInfo(getPackageName(),0);
+            tv_version.setText(packinfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+
+    Timer timer=new Timer();
+    TimerTask timertask =new TimerTask() {
+        @Override
+        public void run() {
+            Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    };
+        timer.schedule(timertask,3000);
     }
 }
+
