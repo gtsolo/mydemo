584a77a43738a29a1701506e407137d79abe8b53
lisi <lee@qq.com>
2019-03-28 23:37:11 +0800

    init

--- demo/src/main/java/com/solo/demo/MainActivity.java
+++ demo/src/main/java/com/solo/demo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.solo.demo;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setView(R.layout.dialoglayout).create().show();
+        /* setContentView(R.layout.activity_main);
         AlertDialog dialog;                           //声明对象
     dialog = new AlertDialog.Builder(this)        //绑定当前界面窗口
              .setTitle("Dialog对话框")             //设置标题
@@ -18,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
                .setPositiveButton("确定", null)     //添加“确定”按钮
                .setNegativeButton("取消", null)     //添加“取消”按钮
          .create();                          //创建对话框
-       dialog.show();                              //显示对话框
+       dialog.show();                              //显示对话框*/
+    }
+
+}
+class Mydialog extends Dialog{
+
+    public Mydialog(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialoglayout);
     }
 }
