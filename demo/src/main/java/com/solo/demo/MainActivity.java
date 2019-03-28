ec98c1bde87413cf7f37ac2e62fb941fdae0dee1
lisi <lee@qq.com>
2019-03-28 22:21:29 +0800

    init

---
+++ demo/src/main/java/com/solo/demo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.solo.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AlertDialog dialog;                           //声明对象
+    dialog = new AlertDialog.Builder(this)        //绑定当前界面窗口
+             .setTitle("Dialog对话框")             //设置标题
+              .setMessage("是否确定退出？")         //设置提示信息
+              .setIcon(R.mipmap.ic_launcher)       //设置图标
+               .setPositiveButton("确定", null)     //添加“确定”按钮
+               .setNegativeButton("取消", null)     //添加“取消”按钮
+         .create();                          //创建对话框
+       dialog.show();                              //显示对话框
+    }
+}
